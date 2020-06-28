6a2a4ae6a0c2af7308e792040785f5f2a8dd0299
Anakin400001 <58471681+Anakin400001@users.noreply.github.com>
2020-06-28 16:51:39 +0300

    Updates

---
+++ precache-manifest.8f9207a12b7836d642790509fd130fa8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb933b05942db7d2ff2c",
+    "url": "./static/css/main.59f3565d.chunk.css"
+  },
+  {
+    "revision": "fb933b05942db7d2ff2c",
+    "url": "./static/js/main.a4a87ba5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "78c48ccf23efcc9a9a70",
+    "url": "./static/css/2.30a9c5ab.chunk.css"
+  },
+  {
+    "revision": "78c48ccf23efcc9a9a70",
+    "url": "./static/js/2.03741af4.chunk.js"
+  },
+  {
+    "revision": "1808b5f6b2dd330757f13c6dc3ddf32a",
+    "url": "./static/media/1233.1808b5f6.ttf"
+  },
+  {
+    "revision": "a4b68b74314b3c629cfac993c20eb275",
+    "url": "./static/media/mask_100.a4b68b74.png"
+  },
+  {
+    "revision": "72893df709d0ed23923491855c6c4140",
+    "url": "./static/media/click.72893df7.png"
+  },
+  {
+    "revision": "d6eea8831b2fb570d839fcfabad55413",
+    "url": "./index.html"
+  }
+];
